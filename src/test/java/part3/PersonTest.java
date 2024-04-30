4e050b1a38b55789a90492696da284090317b00c
Kim Sanghae <pelikan@pusan.ac.kr>
2024-05-01 02:10:03 +0900

    feat(PersonTest.java): Add person's method test

--- src/test/java/part3/PersonTest.java
+++ src/test/java/part3/PersonTest.java
@@ -1,28 +1,44 @@
 package part3;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 class PersonTest {
-    Person person = new Person();
+    Person p = new Person("Kim", 20, "Busan");
+    //NOTE: p는 Person을 가리키는 참조 변수!
+    //NOTE: 객체(인스턴스)는 Heap Memory에 저장된다.
     @Test
     void play() {
-        assertThat(person.play())
+        assertThat(p.play())
                 .isEqualTo("운동을 한다!");
     }
 
     @Test
     void eat() {
-        assertThat(person.eat())
+        assertThat(p.eat())
                 .isEqualTo("음식을 먹다");
     }
 
     @Test
     void walk() {
-        assertThat(person.walk())
+        assertThat(p.walk())
                 .isEqualTo("걷다!");
+    }
+
+    @Test
+    void nameTest() {
+        assertThat(p.name).isEqualTo("Kim");
+    }
+
+    @Test
+    void ageTest() {
+        assertThat(p.age).isEqualTo(20);
+    }
+
+    @Test
+    void addrTest() {
+        assertThat(p.phone).isEqualTo("Busan");
     }
 }
